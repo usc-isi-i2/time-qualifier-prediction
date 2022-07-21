0e25c4b0d51eac30bb0ad57d7d46c1d447e49de7
saggu <amandeep.s.saggu@gmail.com>
2022-07-21 10:46:20 -0700

    update notebooks

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,24 +3,26 @@ import spacy.lang.en
 
 
 def match_dates_based_on_precision(date1: str, wikidata_precision: str, date2: dict):
+    if date2 == "":
+        return  False, ""
     year, month, day = date1.split('-')
-    if wikidata_precision == '10':
+    if wikidata_precision == '10' or wikidata_precision.strip().lower() == 'month':
         return year == str(date2['year']) and month == str(date2['month']), 'month'
-    if wikidata_precision == '9':
+    if wikidata_precision == '9' or wikidata_precision.strip().lower() == 'year':
         return year == str(date2['year']), 'year'
-    if wikidata_precision == '11':
+    if wikidata_precision == '11' or wikidata_precision.strip().lower() == 'day':
         return date1 == date2['date'], 'day'
     return date1 == date2['date'], 'exact'
 
 
-def parse_date(str_with_date: str, nlp: spacy.lang.en.English):
+def parse_date(str_with_date: str, nlp: spacy.lang.en.English, requires=['year', 'month']):
     spacy_docs = nlp(str_with_date)
     spacy_dates = [x for x in spacy_docs.ents if x.label_ == 'DATE']
     parsed_dates = []
     if spacy_dates:
         for sd in spacy_dates:
             parsed_date = dateparser.parse(sd.text, settings={'PREFER_DAY_OF_MONTH': 'first', 
-                                                              'REQUIRE_PARTS': ['year']})
+                                                              'REQUIRE_PARTS': requires})
             if parsed_date:
                 parsed_dates.append({'date': f'{parsed_date.year}-{parsed_date.month:0>2}-{parsed_date.day:0>2}',
                                      'year': parsed_date.year,
